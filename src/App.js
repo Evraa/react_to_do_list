6c0fdfd95e22b37026090f134525241affd4a918
Evraa <evramyousef@gmail.com>
2020-05-06 17:35:59 +0200

    Check/UnCheck todos

--- src/App.js
+++ src/App.js
@@ -24,12 +24,21 @@ class App extends Component{
     ]
   }
 
-
+  markCompleteFinal = (id) => {
+    // console.log(id)
+    //this map function is Godess
+    this.setState({todos: this.state.todos.map(todo => {
+      if (todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })})
+  }
   render(){
     // console.log(this.state.todos)
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markCompleteHere = {this.markCompleteFinal}/>
       </div>
     );
   }
--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 class Todoitem extends Component{
-    render(){
+    
+    
+    //Dynamic Styling
+    getStyle = () => {
+        // if(this.props.todo.completed){
+        //     return {
+        //         textDecoration: 'line-through'
+        //     }
+        // }else{
+        //     return {
+        //         textDecoration: 'none'
+        //     }
+        // }
+
+        //Other method!
+        return {
+            //some styling
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        };
+    }
+        render(){
+        const {id, title } = this.props.todo;
         return (
-            <div>
-                <h2>{this.props.todo.title}</h2>
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" 
+                        onChange = {this.props.markComplete.bind(this, id)}
+                    /> {' '}
+                    {title}
+                </p>
             </div>
         );
     }
 }
+
+Todoitem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+// const itemStyle = {
+//     backgroundColor: '#f4f4f4'
+// }
 
 export default Todoitem;
